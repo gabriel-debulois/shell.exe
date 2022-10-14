f58b2025d59248e3961718ddb7f0ad119ee9e25c
unknown <gabriel.debulois@laplateforme.io>
2022-10-14 16:24:51 +0200

    calculator with less if Job6

--- Job6/my_calculator.sh
+++ Job6/my_calculator.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-
 if [ $2 = x ]
  then
 sum=$(($1 * $3))
 echo "$sum"
 fi
 
-if [ $2 = + ]
+if [ $2 = + ] || [ $2 = - ] || [ $2 = / ]
  then
-sum=$(($1 + $3))
-echo "$sum"
-fi
-
-if [ $2 = - ]
- then
-sum=$(($1 - $3))
-echo "$sum"
-fi
-
-if [ $2 = / ]
- then
-sum=$(($1 / $3))
+sum=$(($1 $2 $3))
 echo "$sum"
 fi
 
